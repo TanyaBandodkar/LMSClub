1bb833372a3fb70c8938b3bdb4ee5da6f2819237
Tanya <tanya@Mrunal.local>
2020-09-12 15:53:53 -0700

    My First Calendar Program

---
+++ Calendar/MyCalendar1.py
@@ -0,0 +1,6 @@
+#My first calendar progam
+print("Day" + "          Time" + "          	Subject")
+print("Monday" + "       10:00 - 11:00 AM" + "	Period 1")
+print("    " + "	     11:30 - 12:30 AM" + "   Math")
+print("Tuesday" + "      9:00 - 11:00 AM" +" 	PE")
+print(" 	" + "     10:00 - 11:00 AM" + "	Science")
