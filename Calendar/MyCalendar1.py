b0da39b8a29a4f7bd28252b737cee669795368b4
Tanya <tanya@Mrunal.local>
2020-09-26 15:27:45 -0700

    modified code

--- Calendar/MyCalendar1.py
+++ Calendar/MyCalendar1.py
@@ -1,6 +1,9 @@
-#My first calendar progam
-print("Day" + "          Time" + "          	Subject")
-print("Monday" + "       10:00 - 11:00 AM" + "	Period 1")
-print("    " + "	     11:30 - 12:30 AM" + "   Math")
-print("Tuesday" + "      9:00 - 11:00 AM" +" 	PE")
-print(" 	" + "     10:00 - 11:00 AM" + "	Science")
+
+
+a="""Day           Time                  Subject
+Monday      10:00 - 11:00 AM         Period 1
+            11:30-12:30 AM.          Math
+Tuesday     9:00-11:00 AM            PE
+            10:00-11:00 AM.          Science"""
+
+print (a)
