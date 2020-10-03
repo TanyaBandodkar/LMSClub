2b4a57d5804d90cad37a555f41b9bf41d3822bed
Tanya <tanya@Mrunal.local>
2020-10-02 21:30:51 -0700

    Step 4 Find Monday without function

---
+++ findMonday(without _function).py
@@ -0,0 +1,19 @@
+
+Mycalendar="""Day           Time             Subject
+                Monday       10:00 - 11:00 AM    Period 1
+                             11:30-12:30 AM      Math
+                Tuesday      9:00-11:00 AM       PE
+                             10:00-11:00 AM.     Science"""
+#Check if string Monday is present using if statement
+if "Monday" in Mycalendar:
+	print("Monday is present in my string")
+else:
+	print("Monday is not present in my string.")
+
+
+print ("The length of my string = " +str(len(Mycalendar)) +" characters")  
+
+
+
+
+                      
